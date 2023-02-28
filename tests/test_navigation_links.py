dcd9ecb8544edcc135ae7acdc414a65fa621d048
qa-apps <pasok.a@yahoo.com>
2023-02-28 12:00:00 -0500

    Update navigation selectors and methods

--- tests/test_navigation_links.py
+++ tests/test_navigation_links.py
@@ -58,3 +58,63 @@ class TestNavigationLinks:
         dropdown_items = page.locator('[role="menu"] a, .dropdown-menu a').all()
         assert len(dropdown_items) > 0, "No items found in Products dropdown"
         
+        # Verify each item has proper attributes
+        for item in dropdown_items:
+            assert item.get_attribute('href'), "Product dropdown item missing href"
+            assert item.text_content(), "Product dropdown item missing text"
+            
+    def test_main_menu_resources_dropdown(self, page: Page):
+        """
+        Test Resources dropdown menu items and navigation
+        """
+        homepage = HomePage(page)
+        homepage.navigate_to()
+        
+        # Open Resources dropdown
+        homepage.click_resources_menu()
+        page.wait_for_timeout(1000)
+        
+        # Get all dropdown items
+        dropdown_items = page.locator('[role="menu"] a, .dropdown-menu a').all()
+        assert len(dropdown_items) > 0, "No items found in Resources dropdown"
+        
+        # Test navigation for first few items
+        for item in dropdown_items[:3]:
+            item_text = item.text_content()
+            item.click()
+            page.wait_for_load_state('networkidle')
+            
+            # Verify navigation occurred
+            assert page.url != homepage.base_url, f"Failed to navigate from Resources: {item_text}"
+            
+            # Navigate back
+            homepage.navigate_to()
+            homepage.click_resources_menu()
+            page.wait_for_timeout(500)
+            
+    def test_main_menu_company_dropdown(self, page: Page):
+        """
+        Test Company dropdown menu items and navigation
+        """
+        homepage = HomePage(page)
+        homepage.navigate_to()
+        
+        # Open Company dropdown
+        homepage.click_company_menu()
+        page.wait_for_timeout(1000)
+        
+        # Get all dropdown items
+        dropdown_items = page.locator('[role="menu"] a, .dropdown-menu a').all()
+        assert len(dropdown_items) > 0, "No items found in Company dropdown"
+        
+        # Verify structure of dropdown items
+        for item in dropdown_items:
+            href = item.get_attribute('href')
+            text = item.text_content()
+            
+            assert text, "Company dropdown item has no text"
+            if href and not href.startswith('#'):
+                assert href.startswith('http') or href.startswith('/'), f"Invalid href format: {href}"
+                
+    def test_footer_navigation_links(self, page: Page):
+        """
--- tests/test_page_structure.py
+++ tests/test_page_structure.py
@@ -17,3 +17,41 @@ class TestPageStructure:
         homepage = HomePage(page)
         homepage.navigate_to()
         
+        # Check for DOCTYPE
+        has_doctype = page.evaluate('() => document.doctype !== null')
+        assert has_doctype, "Page missing DOCTYPE declaration"
+        
+        # Check HTML5 structure
+        html = page.locator('html').first
+        assert html, "HTML element not found"
+        
+        # Check for head and body
+        head = page.locator('head').first
+        body = page.locator('body').first
+        
+        assert head, "HEAD element not found"
+        assert body, "BODY element not found"
+        
+    def test_meta_tags_present(self, page: Page):
+        """
+        Test presence of important meta tags
+        """
+        homepage = HomePage(page)
+        homepage.navigate_to()
+        
+        # Check viewport meta tag
+        viewport = page.locator('meta[name="viewport"]').get_attribute('content')
+        assert viewport, "Viewport meta tag missing"
+        assert 'width' in viewport, "Viewport missing width setting"
+        
+        # Check charset
+        charset = page.locator('meta[charset]').get_attribute('charset')
+        if not charset:
+            charset = page.locator('meta[http-equiv="Content-Type"]').get_attribute('content')
+        assert charset, "Character encoding not specified"
+        
+        # Check description
+        description = page.locator('meta[name="description"]').get_attribute('content')
+        assert description, "Meta description missing"
+        assert len(description) > 50, "Meta description too short"
+        assert len(description) < 160, "Meta description too long"
