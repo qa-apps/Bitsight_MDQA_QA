23123e461eeea2198f73b8d479cc46413faf6532
qa-apps <pasok.a@yahoo.com>
2023-03-11 12:00:00 -0500

    Create XSS prevention test cases

--- tests/test_usability_accessibility.py
+++ tests/test_usability_accessibility.py
@@ -58,3 +58,63 @@ class TestUsabilityAccessibility:
                 
                 # Links should have meaningful text
                 assert text or aria_label, "Link missing accessible text"
+                
+    def test_alt_text_on_images(self, page: Page):
+        """
+        Test that all images have appropriate alt text
+        """
+        homepage = HomePage(page)
+        homepage.navigate_to()
+        
+        images = page.locator('img').all()
+        
+        for img in images:
+            if img.is_visible():
+                alt = img.get_attribute('alt')
+                src = img.get_attribute('src')
+                
+                # Every image should have alt attribute (can be empty for decorative)
+                assert alt is not None, f"Image missing alt attribute: {src}"
+                
+                # Check if alt text is meaningful (not just filename)
+                if alt and src:
+                    filename = src.split('/')[-1].split('.')[0]
+                    assert alt.lower() != filename.lower(), f"Alt text appears to be filename: {alt}"
+                    
+    def test_heading_hierarchy(self, page: Page):
+        """
+        Test proper heading hierarchy for screen readers
+        """
+        homepage = HomePage(page)
+        homepage.navigate_to()
+        
+        # Get all headings
+        headings = page.locator('h1, h2, h3, h4, h5, h6').all()
+        
+        # Should have exactly one H1
+        h1_count = len(page.locator('h1').all())
+        assert h1_count == 1, f"Page should have exactly one H1, found {h1_count}"
+        
+        # Check heading hierarchy
+        previous_level = 0
+        for heading in headings:
+            tag_name = heading.evaluate('el => el.tagName')
+            level = int(tag_name[1])  # Extract number from H1, H2, etc.
+            
+            # Heading levels shouldn't skip (e.g., H1 to H3)
+            if previous_level > 0:
+                assert level <= previous_level + 1, f"Heading hierarchy broken: H{previous_level} to H{level}"
+                
+            previous_level = level
+            
+    def test_form_labels_and_instructions(self, page: Page):
+        """
+        Test form accessibility with labels and instructions
+        """
+        homepage = HomePage(page)
+        homepage.navigate_to()
+        homepage.click_request_demo()
+        
+        page.wait_for_load_state('networkidle')
+        
+        # Check form inputs have labels
--- tests/test_with_real_selectors.py
+++ tests/test_with_real_selectors.py
@@ -12,3 +12,27 @@ class TestWithRealSelectors:
     def test_actual_header_structure(self, page: Page):
         """
         Test with real header selectors from DOM inspection
+        """
+        homepage = HomePageReal(page)
+        homepage.navigate_to()
+        
+        # Using REAL selectors discovered from DOM
+        header_info = homepage.verify_real_header_structure()
+        
+        assert header_info['header_exists'], "Header with class 'site-header' not found"
+        assert header_info['has_transparent_class'], "Header missing 'site-header--transparent' class"
+        assert header_info['nav_exists'], "Navigation element not found in header"
+        
+        # The actual header uses these classes:
+        # - site-header
+        # - site-header--transparent
+        
+    def test_actual_menu_links(self, page: Page):
+        """
+        Test actual menu links with real class names
+        """
+        homepage = HomePageReal(page)
+        homepage.navigate_to()
+        
+        # Get menu items using REAL class: main-menu-block__item-link
+        menu_links = homepage.get_real_menu_links()
