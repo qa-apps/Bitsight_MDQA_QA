754b8d1dd762845815932222bd1c3598970211b5
qa-apps <pasok.a@yahoo.com>
2023-03-18 12:00:00 -0400

    Add button interaction tests

--- tests/test_mobile_responsiveness.py
+++ tests/test_mobile_responsiveness.py
@@ -30,3 +30,51 @@ class TestMobileResponsiveness:
             
             # Check if page loads properly
             assert homepage.is_visible(homepage.hero_title), f"Hero not visible on {device['name']}"
+            
+            # Check for horizontal scrolling (should not exist)
+            has_horizontal_scroll = page.evaluate('''() => {
+                return document.documentElement.scrollWidth > document.documentElement.clientWidth;
+            }''')
+            
+            assert not has_horizontal_scroll, f"Horizontal scroll detected on {device['name']}"
+            
+            # Check if navigation is accessible
+            mobile_menu = page.locator('.mobile-menu, .hamburger, [aria-label="Menu"]').first
+            assert mobile_menu.is_visible() or page.locator('nav').is_visible(), f"Navigation not accessible on {device['name']}"
+            
+    def test_touch_interactions(self, page: Page):
+        """
+        Test touch interactions on mobile
+        """
+        # Set mobile viewport
+        page.set_viewport_size({'width': 375, 'height': 667})
+        
+        homepage = HomePage(page)
+        homepage.navigate_to()
+        
+        # Test tap on mobile menu
+        mobile_menu = page.locator('.mobile-menu, .hamburger, button[aria-label="Menu"]').first
+        
+        if mobile_menu.is_visible():
+            # Simulate tap
+            mobile_menu.tap()
+            page.wait_for_timeout(500)
+            
+            # Menu should open
+            mobile_nav = page.locator('.mobile-nav, .nav-mobile, nav').first
+            assert mobile_nav.is_visible() or page.locator('[role="menu"]').is_visible(), "Mobile menu did not open on tap"
+            
+            # Test swipe gesture (if carousel exists)
+            carousel = page.locator('.carousel, .slider').first
+            if carousel and carousel.is_visible():
+                # Get initial state
+                initial_state = carousel.inner_html()
+                
+                # Simulate swipe
+                box = carousel.bounding_box()
+                if box:
+                    page.mouse.move(box['x'] + box['width'] - 50, box['y'] + box['height'] / 2)
+                    page.mouse.down()
+                    page.mouse.move(box['x'] + 50, box['y'] + box['height'] / 2)
+                    page.mouse.up()
+                    
